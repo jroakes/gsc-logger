adb32d0ff20946e0be4acc1780242d740e4ada58
Ubuntu <ubuntu@ip-172-31-4-68.ec2.internal>
2017-08-30 13:48:26 +0000

    Added 60 second timeout to http requests

--- appengine/utils/utils_auth.py
+++ appengine/utils/utils_auth.py
@@ -12,7 +12,7 @@ import config as cfg
 # Handles authentication using Service Account.
 def get_Auth():
     credentials = ServiceAccountCredentials.from_json_keyfile_name(cfg.CREDENTIAL_SERVICE, cfg.DEFAULT_SCOPES)
-    http = httplib2.Http(memcache)
+    http = httplib2.Http(memcache, timeout=60)
     #http = httplib2.Http()
     return credentials.authorize(http)
     
