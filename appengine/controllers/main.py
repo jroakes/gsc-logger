9a799fa7e57090c3d3b8a37dafa75e87cb1b5623
Matías Battocchia <matias@properati.com>
2017-12-11 14:37:24 -0300

    improved row count readability

--- appengine/controllers/main.py
+++ appengine/controllers/main.py
@@ -16,52 +16,52 @@ import utils.utils_gsc as gsc
 import utils.utils_db as db
 
 from utils.utils_svcdata import ServiceData
-svcdata = ServiceData() 
+svcdata = ServiceData()
 
 from controllers.cron import CronHandler
 from controllers.base import BaseHandler
 
 
-def check_credentials(): 
+def check_credentials():
     return os.path.isfile(cfg.CREDENTIAL_SERVICE)
-    
+
 # Handle / Requests
 class MainHandler(BaseHandler):
 
   def get(self):
-    
+
     sites = None
     sitelist = []
 
     reset = self.request.get('reset','0')
-        
+
     if reset == '1':
         db.delete_entries()
-        message = "Cron database deleted. <a href='/'>Refresh page</a>."      
+        message = "Cron database deleted. <a href='/'>Refresh page</a>."
     elif not check_credentials():
         message = "Upload service credentials JSON from Google App Engine and update filename in config.py file."
     elif cfg.HIDE_HOMEPAGE:
         message = "Nothing to see here."
     else:
-        try: 
+        try:
             message = "Add <strong>{0}</strong> to each Google Search Console account that you want to manage.".format(svcdata['client_email'])
             sites = gsc.list_sites()
             for site in sites:
-                sitelist.append(site + " (Last Save: " + db.last_date(site) + "," + str(db.last_count(site)) + " rows)" )
-                
+                sitelist.append(site + " (Last Save: " + db.last_date(site) + " — " + str(db.last_count(site)) + " rows)" )
+
         except HttpError as e:
-            log.error("GSC Error, {0} ".format(e.content)) 
+            log.error("GSC Error, {0} ".format(e.content))
             message = "GSC Error, {0}. Check Log. ".format(e.content)
-                
+
     variables = {
         'message': message,
         'sites': sitelist
         }
-    
+
     self.render_response('root.html', **variables)
 
-         
-    
+
+
 app = webapp2.WSGIApplication(
     [
      (r'/', MainHandler),
